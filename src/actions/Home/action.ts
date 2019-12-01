5753960e5d22cb50fafccb305b6fdbcd53df25ff
Mehran Ganjgahi <M.ganjgahi@haj.ir>
2019-12-01 13:10:01 +0330

    Home Api

--- src/actions/Home/action.ts
+++ src/actions/Home/action.ts
@@ -1,9 +1,17 @@
 import { AppAction } from "../../store/state";
 import { HomeActionTypes } from "./actionType";
 import { ActionModel } from "./model";
+import {HomeApi} from './api'
 
 export const HomeActions = {
-    testAction: (): AppAction<ActionModel> => (dispatch, getState) => {
+    testAction: (): AppAction<ActionModel> => async (dispatch, getState) => {
+        try {
+            const res = await HomeApi.testApi()
+            console.log(res.status, " / ", res.data) 
+        } catch (error) {
+            console.log(error)
+        }
         dispatch({ type: HomeActionTypes.TestAction  });
+
     },
 };
