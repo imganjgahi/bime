fc20d2c9af9cc688ac77b1d318e26779c16986d7
Mehran Ganjgahi <M.ganjgahi@haj.ir>
2019-12-01 17:27:10 +0330

    Modal Component

--- src/Components/Home/Landingpanel.tsx
+++ src/Components/Home/Landingpanel.tsx
@@ -4,22 +4,40 @@ import { HomeActions } from '../../actions/Home/action';
 import motor from '../../assets/images/motorcycle.svg'
 import carBody from '../../assets/images/carBody.svg'
 import thirdparty from '../../assets/images/thirdparty.svg'
-
+import Modal from '../../Utils/Modals/Modals'
 
 type IProps = IHomeState & typeof HomeActions;
 
 type IState = {
-    
+    motorModal: boolean
 };
 
 class Landingpanel extends React.Component<IProps, IState> {
-    state = {  }
+    constructor(props: IProps){
+        super(props);
+        this.state ={
+            motorModal: false
+        }
+    }
+    motorModalHandler = (status: boolean) => {
+        this.setState({motorModal: status})
+    }
     render() {
         return (
-            <div className="landingPanel">
-                <div className="panel">
+            <React.Fragment>
+                <Modal 
+                // type= "warning"
+                onCancel = {() => this.motorModalHandler(false)}
+                title="بیمه موتور سیکلت"
+                visiblity={this.state.motorModal}>
+                    <h1>
+                        بیمه بدنه موتور سیکلت
+                    </h1>
+                </Modal>
+                <div className="landingPanel">
+                <div className="panel" onClick={() => this.motorModalHandler(true)}>
                     <img className="panelIcon" src={motor} alt="موتور سیکلت"/>
-                    <small>بیمه موتور</small>
+                    <small>موتور سیکلت</small>
                 </div>
                 <div className="panel">
                     <img className="panelIcon" src={carBody} alt="بدنه"/>
@@ -30,8 +48,8 @@ class Landingpanel extends React.Component<IProps, IState> {
                     <small>شخص ثالث</small>
                 </div>
             </div>
+            </React.Fragment>
         );
     }
 }
-
 export default Landingpanel;
---
+++ src/Utils/Modals/Modals.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+interface IProps {
+    noHeader?: boolean
+    title?: string
+    type?: string
+    onOk?: () => void
+    onCancel: () => void
+    visiblity: boolean
+    children?: any
+}
+interface IState {
+    isActive: boolean;
+}
+class Modal extends React.Component<IProps, IState> {
+
+    constructor(props: IProps){
+        super(props)
+        this.state = {
+            isActive: false
+        }
+    }
+
+    componentDidUpdate(prevProps: IProps){
+        if(prevProps.visiblity === false && this.props.visiblity && !this.state.isActive) {
+            setTimeout(() => {
+                this.setState({isActive: true})
+            }, 200);
+        }
+    }
+    
+    closeHandler = () => {
+        this.setState({isActive: false})
+        setTimeout(() => {
+                this.props.onCancel()
+        }, 200);
+    }
+    render() {
+    let modalClassName = this.props.type ? `simpleModal ${this.props.type}` : "simpleModal";
+        modalClassName = this.state.isActive ? modalClassName + ' active' : modalClassName;
+    if (!this.props.visiblity) {
+        return null
+    }
+
+    return (
+        <div className="modalContainer" onClick = {() => {
+            this.closeHandler()
+        }}>
+            <div className={modalClassName}>
+            {!this.props.noHeader && (
+                <div className="modalHeader">
+                    {this.props.title ? this.props.title : "پیغام"}
+                </div>
+            )}
+            <div className="modalBody">
+                {this.props.children}
+            </div>
+            <div className="modalFooter">
+                {this.props.onOk && <button onClick={() => {
+                    if (this.props.onOk) {
+                        this.props.onOk()
+                    }
+                }}>
+                    تایید
+                    </button>}
+                <button onClick={() =>   this.closeHandler() }>
+                    بستن
+                </button>
+            </div>
+        </div>
+        </div>
+    )
+    }
+
+}
+
+export default Modal
